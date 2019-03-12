4ebf3ab0ef756f83ded5671c58d9b5ffa174cf25
Ona Demuytere <onademuy@student.arteveldehs.be>
2019-03-12 15:52:44 +0100

    update

--- tinder/index.py
+++ tinder/index.py
@@ -9,23 +9,23 @@ sense = SenseHat()
 # tinder app
 def tinder():
     request = requests.get('https://randomuser.me/api').json()
-     with open('data.json', 't') as json_data:
+    with open('data.json', 'a') as json_data:
         json.dump(request, json_data)
 
-    global data = request['results'][0]
-    global name = data['name']['first']
+    data = request['results'][0]
+    name = data['name']['first']
     sense.show_message(name)
 tinder()
 
 while 1:
     for event in sense.stick.get_events():
         if event.action =="pressed":
-            if even.direction == 'left':
+            if event.direction == 'left':
                 sense.clear(255,0,0)
                 sense.show_message('disliked')
             elif event.direction == "right":
                 sense.clear(0,255,0)
                 sense.show_message('liked')
-tinder()
-sleep(1)
-sense.clear()
+    tinder()
+    sleep(1)
+    sense.clear()
